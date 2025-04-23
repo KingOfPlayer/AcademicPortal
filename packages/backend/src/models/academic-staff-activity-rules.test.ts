eb9035260d1304d55271867901834c203b34a96a
Erkan HAZIR <41erkan2002@gmail.com>
2025-04-23 16:39:01 +0300

    activity rules validate, test

--- packages/backend/src/models/academic-staff-activity-rules.test.ts
+++ packages/backend/src/models/academic-staff-activity-rules.test.ts
@@ -2,15 +2,29 @@ import { describe, it, expect } from "vitest";
 import { AcademicStaffActivityRules } from "./academic-staff-activity-rules";
 import { StaffPosition } from "./academic-staff-disicpline-utils";
 
-describe("Academic Staff Activity Rules", () => {
+describe("Academic Staff Activity Rule", () => {
   it("Constructor Test", async () => {
-    const academicStaffActivityRules =
+    const rule =
       new AcademicStaffActivityRules({
         expression: "A1-A2",
         positionType: StaffPosition.AssociateProfessor,
         minimumCount: 200,
       });
-
-    expect(academicStaffActivityRules.expression).toBe("A1-A2");
+    
+      await expect(rule.validate()).resolves.not.toThrow();
+  
+      rule.expression = "A1-A.4";
+      await expect(rule.validate()).rejects.toThrow();
+  
+      rule.expression = "A.1-A4";
+      await expect(rule.validate()).rejects.toThrow();
+  
+      rule.expression = "A4-A1";
+      await expect(rule.validate()).rejects.toThrow();
+  
+      rule.expression = "A1-A4";
+  
+      rule.minimumCount = 0;
+      await expect(rule.validate()).rejects.toThrow();
   });
 });
