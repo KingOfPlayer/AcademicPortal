31407cecb6d4ba48ee7ce8aedfd9f9e2f4cfa9b3
ilberozgurdayi <73353700+ilberozgurdayi@users.noreply.github.com>
2025-04-26 17:19:51 +0300

    view error
    abi şu hataya bir bakar mısın sana zahmet

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -2,16 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginPage from '@/views/LoginPage.vue'
 import DashboardView from '../views/DashboardView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
     {
-      path:'/loginpage',
-      name:'LoginPage',
-      component:LoginPage,
-
-
+      path: '/loginpage',
+      name: 'LoginPage',
+      component: LoginPage,
     },
     {
       path: '/',
